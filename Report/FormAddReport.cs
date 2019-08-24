9a02bcd764b7dc4cd233b83d46654a68a8becbcf
Dmitriy Po <potemkin.d.u@yandex.ru>
2019-08-24 14:06:27 +0500

    Добавлен коментарий

--- Report/FormAddReport.cs
+++ Report/FormAddReport.cs
@@ -17,6 +17,7 @@ namespace Report
 
         private void buttonSave_MouseHover (object sender, EventArgs e)
         {
+        	//добавлен небольшой коментарий
             ToolTip t = new ToolTip();
             t.SetToolTip(buttonSave, "Сохранить");
         }
